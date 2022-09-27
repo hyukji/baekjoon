6f2af383004747cf9b225cee153f5382f00a191c
hyukji <wlgur0416@dgist.ac.kr>
2022-09-27 17:11:06 +0900

    220927

--- c++/sandbox.cpp
+++ c++/sandbox.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
-
 using namespace std;
+
+void swap(int* x, int* y);
 
 int main()
 {
-    int num;
-    cin >> num;
+    int num1 = 5;
+    int num2 = 10;
 
-    cout << "들어온 수는 " << num << endl;
+    cout << "num1 " << num1 << "\n";
+    cout << "num2 " << num2 << "\n";
+    
+    swap(&num1, &num2);
+
+    cout << "after swap\n";
+    cout << "num1 " << num1 << "\n";
+    cout << "num2 " << num2 << "\n";
 
     return 0;
+}
+
+
+void swap(int* px, int* py)
+{
+    int tmp;
+
+    tmp = *px;
+    *px = *py;
+    *py = tmp;
 }
